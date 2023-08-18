e658a080a0aa97e2f8a1f8ee938d736d8885ce4e
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-08-18 13:14:11 +0330

    Implement react router for 2 pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './style.css';
+import {Routes, Route} from "react-router-dom";
+import Navbar from './Components/Navbar';
+import Books from "./Components/Books";
+import Form from "./Components/Form";
 
 const App = ()=> {
   return (
-    <div className="container">
-    </div>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Books />}/>
+        <Route path='/categories' element={<Form />}/>
+      </Routes>
+    </>
   );
 }
 
