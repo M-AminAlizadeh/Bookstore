317ad1ea5378b890c83a7e55dd1e1d1ec873b45f
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-08-23 16:47:44 +0330

    Add get data from server and display it to the user

--- src/Components/Book.jsx
+++ src/Components/Book.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from "react-redux";
-import { remove } from "../Redux/books/booksSlice";
+import { removeBook, fetchBooks } from "../Redux/books/booksSlice";
 
 const Book = ({ id , title , author , category}) => {
   const dispatch = useDispatch();
+
+  const handleRemove = () => {
+    dispatch(removeBook({ itemId: id }));
+    dispatch(fetchBooks());
+  };
 
   return (
     <section className="book">
       <h3>{title}</h3>
       <h5>{author}</h5>
       <p>{category}</p>
-      <button type='submit' onClick={() => {
-          dispatch(remove({ id }));
-        }}>
+      <button type='submit' onClick={handleRemove}>
         Remove
         </button>
     </section>
