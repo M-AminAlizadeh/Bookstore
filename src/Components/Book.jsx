b5e880acbb56bbd45d11b6be7cfd7e0e1eebfc1c
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-08-23 10:03:17 +0330

    Add remove button functionality

--- src/Components/Book.jsx
+++ src/Components/Book.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from "react-redux";
+import { remove } from "../Redux/books/booksSlice";
 
-const Book = ({ name, author }) => {
+const Book = ({ id , title , author , category}) => {
+  const dispatch = useDispatch();
+
   return (
     <section className="book">
-      <h3>{name}</h3>
+      <h3>{title}</h3>
       <h5>{author}</h5>
-      <button>Remove</button>
+      <p>{category}</p>
+      <button type='submit' onClick={() => {
+          dispatch(remove({ id }));
+        }}>
+        Remove
+        </button>
     </section>
   );
 };
 
 Book.propTypes = {
-  name: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
 };
 
 export default Book;
