c3fe5f1109cd5b9ef70aeb390df163759040ea3f
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-08-23 16:46:46 +0330

    Add unique id

--- src/Pages/Books.jsx
+++ src/Pages/Books.jsx
@@ -1,11 +1,13 @@
 import Books from "../Components/Books";
 import Form from "../Components/Form";
+import UniqueId from '../Components/UniqueId';
 
 const BooksPage = () =>{
   return(
   <div className="books-page">
     <Books/>
     <Form/>
+    <UniqueId />
   </div>)
 }
 
