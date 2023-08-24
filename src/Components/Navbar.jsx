e38be3caf08a5813f2b375a061e4a6fede8bcf58
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-08-24 10:35:58 +0330

    Add style to navbar component

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,19 @@
 import { Link } from 'react-router-dom';
+import styles from "./navbar.module.css";
 
 const Navbar = () => {
   return (
-  <nav>
-    <h1>Bookstore CMS</h1>
-    <ul>
-      <li><Link to="/">Books</Link></li>
-      <li><Link to="/categories">Categories</Link></li>
-    </ul>
+  <nav className={styles.navbar}>
+    <div className={styles.rightSide}>
+      <h1 className={styles.brandName}>Bookstore CMS</h1>
+      <ul className={styles.linksContainer}>
+        <li><Link to="/" className={styles.link}>Books</Link></li>
+        <li><Link to="/categories" className={styles.link}>Categories</Link></li>
+      </ul>
+    </div>
+    <div className={styles.profileContainer}>
+      <img src="https://img.icons8.com/ios-filled/0290ff/20/user.png" alt="user"/>
+    </div>
   </nav>)
 }
 
