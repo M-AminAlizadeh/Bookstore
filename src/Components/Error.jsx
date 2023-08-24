634f9c70359c67dce3d6b4f548a35b7814e07f13
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-08-24 12:55:05 +0330

    Build error component and style it

---
+++ src/Components/Error.jsx
@@ -0,0 +1,9 @@
+import styles from "./error.module.css";
+
+const Error = ()=>{
+  return <span className={styles.errorMessage}>
+    Please fill all the inputs
+  </span>
+}
+
+export default Error;
